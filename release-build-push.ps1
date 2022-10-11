a333376cec687a93ef4902b4175efa2f83fc5b73
Gunnar Már Óttarsson <gunni@starcraft.is>
2022-10-11 16:54:23 +0000

    script now pauses after exec

--- release-build-push.ps1
+++ release-build-push.ps1
@@ -1,9 +1,15 @@
 dotnet restore .
+
 Remove-Item *.nupkg
 Remove-Item *.snupkg
+
 dotnet pack --include-symbols -c Release -o .
+
 $pkg = gci *.nupkg 
 nuget push $pkg -Source https://www.nuget.org/api/v2/package -NonInteractive -SkipDuplicate
-$snupkg = gci *.snupkg 
+
 Write-Host Pushing symbols to https://www.nuget.org/api/v2/package
+$snupkg = gci *.snupkg 
 nuget push $snupkg -Source https://www.nuget.org/api/v2/package -NonInteractive -verb
+
+pause
